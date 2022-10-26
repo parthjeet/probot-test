1879c9421a0ce0c58daa5db2e1a2d23f251bbe07
Parthjeet <parthjeet25@gmail.com>
2022-10-26 14:42:50 +0100

    Update index_2.js

--- index_2.js
+++ index_2.js
@@ -9,14 +9,16 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
   
   app.on(['pull_request.opened', 'pull_request.edited','pull_request.reopened', 'pull_request.ready_for_review','pull_request.synchronize','pull_request.labeled'], async (context) =>{
-
+    let release_contract_yamp_obj, repo_json
+    var release_contract_user_stories_array = new Array()
+    var pr_user_stories_list = new Array()
     try {
       // Release Contract stuff below
       // parse release contract file
-      const repo_json = context.repo({ path: 'release-contract.yml' }); //this gives you the path, owner of the source repo
+      repo_json = context.repo({ path: 'release-contract.yml' }); //this gives you the path, owner of the source repo
       const release_contract_content_response_json = await context.octokit.repos.getContent({ owner: `${repo_json.owner}`, repo: `${repo_json.repo}`, path: 'release-contract.yml' });
       const release_contract_content_utf8 = Buffer.from(release_contract_content_response_json.data.content, 'base64').toString('utf8')
-      const release_contract_yamp_obj = yaml.load(release_contract_content_utf8)
+      release_contract_yamp_obj = yaml.load(release_contract_content_utf8)
     } catch (error) {
       app.log.debug(error)
       const issueComment = context.issue({
@@ -28,7 +30,6 @@ module.exports = (app) => {
     try{
       var release_name_string = release_contract_yamp_obj['Release']
       const release_contract_features_array = Array.from(release_contract_yamp_obj['Features'])
-      var release_contract_user_stories_array = new Array()
       release_contract_features_array.forEach(feature => {
         var feature_user_stories_array = Array.from(feature['UserStories'])
         feature_user_stories_array.forEach(user_story =>{
@@ -47,8 +48,10 @@ module.exports = (app) => {
       // read PR content- title and body
       try{
       const pr_content_payload = context.payload.pull_request.title.concat('\n', context.payload.pull_request.body)
+      
+      
       // search for artifact pattern
-      const pr_user_stories_list = Array.from(getUserStoriesList(pr_content_payload))
+      pr_user_stories_list = Array.from(getUserStoriesList(pr_content_payload))
       } catch(error) {
         app.log.debug(error)
         const issueComment = context.issue({
@@ -64,7 +67,7 @@ module.exports = (app) => {
       let result = await chekLogic(context, release_contract_user_stories_array, pr_user_stories_list)
 
       // Update Check with Resolution
-      await resolveCheck(context, checkRun, result, repo_json.owner, repo_json.repo)
+      await resolveCheck(context, checkRun, result, repo_json.owner, repo_json.repo, app)
       }catch(error){
         app.log.debug(error)
         await resolveCheck(context, checkRun, 'skipped', repo_json.owner, repo_json.repo)
@@ -96,11 +99,14 @@ async function createCheckRun(context, source_repo_owner, source_repo) {
   })
 }
 
-async function chekLogic(context, rc_us_array, pr_us_array) {
+async function chekLogic(context, rc_us_array, pr_us_array, app) {
   const filteredArray = Array.from(rc_us_array.filter(value => pr_us_array.includes(value)));
-  if(filteredArray.length > 0){
+  if(filteredArray.length > 0 && filteredArray.length == pr_us_array.length){
     return "success"
   }
+  
+  
+  
   return "failure"
 }
 
@@ -113,7 +119,7 @@ async function resolveCheck(context, checkRun, result, source_repo_owner, source
     },
     owner: source_repo_owner,
     repo: source_repo,
-    name: "Probot CI Test",
+    name: "release-contract probot check",
     check_run_id: checkRun.data.id,
     status: "completed",
     head_sha: headSha,
@@ -131,6 +137,6 @@ async function resolveCheck(context, checkRun, result, source_repo_owner, source
  * @returns {Array} Array of user stories present in the message string
  */
 function getUserStoriesList(message){
-   const user_stories_regex = /[US|us].[0-9]*/gm;
+   const user_stories_regex = /(US|us).[0-9]*/gm;
    return message.match(user_stories_regex);
 }
